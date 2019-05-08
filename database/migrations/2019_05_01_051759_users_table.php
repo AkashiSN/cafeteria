367a47370e55f9e5d1527ac221be4e1ce5433547
AkashiSN <btorntireinvynriy@gmail.com>
2019-05-08 23:44:08 +0900

    fix logout issue

--- database/migrations/2019_05_01_051759_users_table.php
+++ database/migrations/2019_05_01_051759_users_table.php
@@ -14,10 +14,10 @@ class UsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->rememberToken();
             $table->bigIncrements('user_id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
