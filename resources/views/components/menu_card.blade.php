116b9de611047954b84ba996e15b94936f53d2de
Kazuya Izumi <shikibu9419@gmail.com>
2019-06-14 12:14:37 +0900

    [update] improve home and componentize menu_card

---
+++ resources/views/components/menu_card.blade.php
@@ -0,0 +1,57 @@
+<div class="card">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-sm-10">
+                <h4 class="card-title">{{ $menu['menu'] -> item_name }}</h4>
+            </div>
+            <div class="col-sm-2">
+                はぁと
+            </div>
+        </div>
+
+        <div class="row mb-2">
+            <div class="col-sm-2">
+                <p class="card-text">¥{{ $menu['menu'] -> price }}</p>
+            </div>
+            <div class="col-sm-3">
+                お星様
+            </div>
+        </div>
+
+        <div class="container-fluid">
+            <div class="row flex-row flex-nowrap">
+                <div class="col-3">
+                    <img src="https://park.ajinomoto.co.jp/wp-content/uploads/2018/03/710131.jpeg" width="150" height="100" />
+                </div>
+                <div class="col-3">
+                    <img src="https://park.ajinomoto.co.jp/wp-content/uploads/2018/03/710131.jpeg" width="150" height="100" />
+                </div>
+                <div class="col-3">
+                    <img src="https://park.ajinomoto.co.jp/wp-content/uploads/2018/03/710131.jpeg" width="150" height="100" />
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-15">
+            <div class="col-sm-10">
+                <div class="row">
+                    <div class="col-sm-2 font-weight-bold card-text">エネルギー</div>
+                    <div class="col-sm-2 card-text">{{ $menu['menu'] -> energy }} kcal</div>
+                    <div class="col-sm-2 font-weight-bold card-text">脂質</div>
+                    <div class="col-sm-2 card-text">{{ sprintf('%.1f', $menu['menu'] -> lipid) }} g</div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2 font-weight-bold card-text">タンパク質</div>
+                    <div class="col-sm-2 card-text">{{ sprintf('%.1f', $menu['menu'] -> protein) }} g</div>
+                    <div class="col-sm-2 font-weight-bold card-text">塩分</div>
+                    <div class="col-sm-2 card-text">{{ sprintf('%.1f', $menu['menu'] -> salt) }} g</div>
+                </div>
+            </div>
+            @if ($is_served)
+            <div class="col-sm-2">
+                <button type="button" class="btn btn-success" id="button-sold-out">提供中</button>
+            </div>
+            @endif
+        </div>
+    </div>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,10 +20,10 @@
     @endif
 </ul>
 
-<div class="container ph-20 mt-10">
+<div class="container mt-10 ph-70">
     <div class="row">
         <div class="col-4">
-            <select class="form-control" id="exampleFormControlSelect1">
+            <select class="form-control">
                 <option>7/1〜7/5</option>
                 <option>7/15〜7/12</option>
                 <option>7/15〜7/19</option>
@@ -33,69 +33,12 @@
         </div>
     </div>
 
-    <div class="container mt-10">
-        <p>7月1日</p>
-
-        @foreach ($menus as $menu)
-        <p class="text-justify text-muted">{{ $menu['description'] }}</p>
-
-        <div class="card mb-3 menu">
-            <a href="{{ route('menu.detail', ['menu_id' => $menu['menu'] -> menu_id]) }}"></a>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-sm-8">
-                        <h4 class="card-title">{{ $menu['menu'] -> item_name }}</h4>
-                    </div>
-                    <div class="col-sm-2">
-                        はぁと
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-sm-2">
-                        <h4 class="text-in-card">{{ $menu['menu'] -> price}} Yen</h4>
-                    </div>
-                    <div class="col-sm-2">
-                        お星様
-                    </div>
-                </div>
-
-                <div class="container-fluid">
-                    <div class="row flex-row flex-nowrap">
-                        <div class="col-2">
-                            <img src="https://park.ajinomoto.co.jp/wp-content/uploads/2018/03/710131.jpeg" width="150" height="100" />
-                        </div>
-                        <div class="col-2">
-                            <img src="https://park.ajinomoto.co.jp/wp-content/uploads/2018/03/710131.jpeg" width="150" height="100" />
-                        </div>
-                        <div class="col-2">
-                            <img src="https://park.ajinomoto.co.jp/wp-content/uploads/2018/03/710131.jpeg" width="150" height="100" />
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row mt-15">
-                    <div class="col-sm-8">
-                        <div class="row">
-                            <div class="col-sm-2 font-weight-bold text-in-card">エネルギー</div>
-                            <div class="col-sm-2 text-in-card">{{ $menu['menu'] -> energy }} kcal</div>
-                            <div class="col-sm-2 font-weight-bold text-in-card">脂質</div>
-                            <div class="col-sm-2 text-in-card">{{ sprintf('%.1f', $menu['menu'] -> lipid) }} g</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-2 font-weight-bold text-in-card">タンパク質</div>
-                            <div class="col-sm-2 text-in-card">{{ sprintf('%.1f', $menu['menu'] -> protein) }} g</div>
-                            <div class="col-sm-2 font-weight-bold text-in-card">塩分</div>
-                            <div class="col-sm-2 text-in-card">{{ sprintf('%.1f', $menu['menu'] -> salt) }} g</div>
-                        </div>
-                    </div>
-                    <div class="col-sm-2">
-                        <button type="button" class="btn btn-success btn-lg">提供中</button>
-                    </div>
-                </div>
-            </div>
+        <p class="mt-10">7月1日</p>
+        <div class="container mt-10 ph-70">
+            @foreach ($menus as $menu)
+            <p class="text-justify text-muted mt-3">{{ $menu['description'] }}</p>
+            @include('components.menu_card', ['is_served' => true])
+            @endforeach
         </div>
-        @endforeach
-    </div>
 </div>
 @endsection
