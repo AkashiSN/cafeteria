25507f4684d09f57dfb4134fe4cfb93be3db49d6
kazu096 <e1514@s.akashi.ac.jp>
2019-06-26 18:26:22 +0900

    unti

---
+++ resources/views/error.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.default')
+
+@section('content')
+<!-- Content section -->
+<section class="py-5">
+    <div class="error">
+        <h1>予期せぬエラーが起きました</h1>
+        <p class="lead"></p>
+        <p>an error occured</p>
+    </div>
+</section>
+@endsection
