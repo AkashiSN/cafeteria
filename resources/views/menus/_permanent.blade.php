f65b02a524b7b46c12f5c61b07d79af426f1c84a
AkashiSN <btorntireinvynriy@gmail.com>
2019-07-22 12:15:16 +0900

    [update] reduce api call

--- resources/views/menus/_permanent.blade.php
+++ resources/views/menus/_permanent.blade.php
@@ -3,7 +3,7 @@
         <p class="text-justify text-muted">{{ $menus['description'] }}</p>
         @foreach($menus['menus'] as $menu)
             <div>
-                <menu-card :menu="{{ $menu }}" :base-route="'{{ url("") }}'" :is-liked="{{ var_export($menu -> isLiked(), true) }}" :valid-favorite-button="{{ var_export(Auth::check(), true) }}" />
+                <menu-card :menu="{{ $menu }}" :base-route="'{{ url("") }}'" :is-liked="{{ var_export($menu -> isLiked(), true) }}" :have-image="{{ var_export($menu -> haveImage(), true) }}" :valid-favorite-button="{{ var_export(Auth::check(), true) }}" />
             </div>
         @endforeach
     </div>
