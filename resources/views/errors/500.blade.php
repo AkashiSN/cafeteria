a30a1d2299a0faf72c235c917ac59f035b01eaf9
AkashiSN <btorntireinvynriy@gmail.com>
2019-07-23 13:57:15 +0900

    [update] error page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default')
+
+@section('content')
+<!-- Content section -->
+<div class="container" style="text-align: center;">
+    <img src="{{ url('') }}/images/karameru.jpg" style="border-radius: 50%; width: 40%;" />
+    <h1>500 Internal Server Error</h1>
+    <p class="text-center">Copyright &copy; からめる, @purinharumaki</p>
+</div>
+
+@endsection
